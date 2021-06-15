50e7cfbf1a35b9f700328875a027473b741d96b9
aziyan99 <rajaazian08@gmail.com>
2021-06-15 15:15:30 +0700

    Initial Commit

---
+++ BoBNolepAssique/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoBNolepAssique
+//
+//  Created by Raja Azian on 15/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
