5ad3d482a7aa188886f1ffece4c6afe510ab56f1
aziyan99 <rajaazian08@gmail.com>
2021-06-17 07:33:43 +0700

    refactoring story board and add new viewController files

---
+++ BoBNolepAssique/KeinginanViewController.swift
@@ -0,0 +1,17 @@
+//
+//  KeinginanViewController.swift
+//  BoBNolepAssique
+//
+//  Created by Raja Azian on 17/06/21.
+//
+
+import UIKit
+
+class KeinginanViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Keinginan"
+    }
+
+}
---
+++ BoBNolepAssique/PengaturanViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PengaturanViewController.swift
+//  BoBNolepAssique
+//
+//  Created by Raja Azian on 17/06/21.
+//
+
+import UIKit
+
+class PengaturanViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pengaturan"
+    }
+
+}
