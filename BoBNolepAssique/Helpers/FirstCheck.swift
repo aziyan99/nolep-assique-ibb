a45dbcb1c61f46e41b7b78854c5669dfc084668f
aziyan99 <rajaazian08@gmail.com>
2021-06-17 08:03:11 +0700

    add new view controller and his storyboard

---
+++ BoBNolepAssique/Helpers/FirstCheck.swift
@@ -0,0 +1,14 @@
+//
+//  FirstCheck.swift
+//  BoBNolepAssique
+//
+//  Created by Raja Azian on 17/06/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+func isFirstOpenApp () -> Bool {
+    return true
+}
---
+++ BoBNolepAssique/InvestasiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InvestasiViewController.swift
+//  BoBNolepAssique
+//
+//  Created by Raja Azian on 17/06/21.
+//
+
+import UIKit
+
+class InvestasiViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
